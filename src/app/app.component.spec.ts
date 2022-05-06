4c81235e62015f71bceae348a52c139bc1dfc516
Diego Albitres <diego.albitres@gmail.com>
2022-05-06 06:40:01 -0500

    chore: replaced references to template name
    * Replaced all references to angular-starter with the project name, waw-project-angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-starter'`, () => {
+  it(`should have as title 'waw-frontend-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual("angular-starter");
+    expect(app.title).toEqual("waw-frontend-angular");
   });
 
   it("should render title", () => {
@@ -27,7 +27,7 @@ describe("AppComponent", () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector(".content span")?.textContent).toContain(
-      "angular-starter app is running!"
+      "waw-frontend-angular app is running!"
     );
   });
 });
