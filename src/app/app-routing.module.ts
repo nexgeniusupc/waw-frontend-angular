15a53398c06b6da68cba5ef2fd75a3a55aa2764a
Diego Albitres <diego.albitres@gmail.com>
2022-05-07 01:54:02 -0500

    feat: link signin component to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ProfileComponent } from "./profile/profile.component";
+import { SignInComponent } from "./auth/pages/signin/signin.component";
 import { SignUpComponent } from "./auth/pages/signup/signup.component";
 
 const routes: Routes = [
   { path: "", component: ProfileComponent },
+  { path: "account/signin", component: SignInComponent },
   { path: "account/signup", component: SignUpComponent },
 ];
 
