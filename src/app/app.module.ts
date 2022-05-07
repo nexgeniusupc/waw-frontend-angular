53af68503a7c9c2423d40ca0d22cac8dd6b7b2b5
Angel Meneses <angelmenesest123@gmail.com>
2022-05-07 01:01:56 -0500

    chore: renamed and organized files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { SignUpComponent } from "./sign-up/sign-up.component";
+import { SignUpComponent } from "./auth/pages/signup/signup.component";
 import { ProfileComponent } from "./profile/profile.component";
 
 type ImportedModule = Type<any> | ModuleWithProviders<{}> | any[];
