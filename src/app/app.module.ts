4ce5624e46631497023fe3d923a40fdc2666e533
Kolya Castañeda <gakol8@hotmail.com>
2022-05-07 00:17:48 -0500

    feat(ui): added sign-in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SignInComponent } from "./sign-in/sign-in.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SignInComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
