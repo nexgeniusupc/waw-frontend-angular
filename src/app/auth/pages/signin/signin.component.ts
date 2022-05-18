dd3342f0bdbdec3289c80010f86891ecb66dfaae
Diego Albitres <diego.albitres@gmail.com>
2022-05-18 06:06:33 -0500

    feat: initial implementation of new AuthService

--- src/app/auth/pages/signin/signin.component.ts
+++ src/app/auth/pages/signin/signin.component.ts
@@ -1,8 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Subscription } from "rxjs";
+import { Nullable } from "src/app/common/utils/types";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: "app-signin",
   templateUrl: "./signin.component.html",
   styleUrls: ["./signin.component.css"],
 })
-export class SignInComponent {}
+export class SignInComponent implements OnInit, OnDestroy {
+  email = "";
+  subscription: Nullable<Subscription> = null;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.subscription = this.authService.user.subscribe(user => {
+      if (user) {
+        this.router.navigate(["/"]);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  handleLogin() {
+    if (this.email.length > 0) {
+      this.authService.login(this.email);
+    }
+  }
+}
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Subscription } from "rxjs";
+import { Project, User } from "../auth/model/user";
 import { AuthService } from "../auth/services/auth.service";
-import { User } from "../auth/model/user";
+import { ProjectsService } from "../auth/services/projects.service";
+import { Nullable } from "../common/utils/types";
 
 @Component({
   selector: "app-profile",
   templateUrl: "./profile.component.html",
   styleUrls: ["./profile.component.css"],
 })
-export class ProfileComponent implements OnInit {
-  user: User | null = null;
+export class ProfileComponent implements OnInit, OnDestroy {
+  user: Nullable<User> = null;
+  projects: Project[] = [];
+  subscriptions: Subscription[] = [];
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private projectsService: ProjectsService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.authService.login();
-    this.user = this.authService.getUser();
+    this.subscriptions.push(
+      this.authService.user.subscribe(user => {
+        this.user = user;
+      })
+    );
+    if (this.user === null) {
+      this.router.navigate(["/account/signin"]);
+      return;
+    }
+    this.subscriptions.push(
+      this.projectsService.getByUser(this.user.id).subscribe(projects => {
+        this.projects = projects;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    for (const sub of this.subscriptions) {
+      sub.unsubscribe();
+    }
   }
 }
