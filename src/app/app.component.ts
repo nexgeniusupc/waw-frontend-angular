5c5e420d2c5ff29d62926c1833e8409a9acb4752
Diego Albitres <diego.albitres@gmail.com>
2022-05-26 07:39:21 -0500

    feat: replace MatIcons with MatSymbols
    * This is the new version of icons from the Material team
    * This package includes filled and outlined icons with a variable font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { MatIconRegistry } from "@angular/material/icon";
 
 @Component({
   selector: "app-root",
@@ -7,4 +8,12 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "waw-frontend-angular";
+
+  constructor(private iconRegistry: MatIconRegistry) {
+    iconRegistry.registerFontClassAlias(
+      "material-symbols-outlined",
+      "material-symbols-outlined"
+    );
+    iconRegistry.setDefaultFontSetClass("material-symbols-outlined");
+  }
 }
