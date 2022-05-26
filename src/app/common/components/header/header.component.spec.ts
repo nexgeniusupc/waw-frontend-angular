e3a04e7a8d3a30f52f91bf01f357b000a2d420af
Diego Albitres <diego.albitres@gmail.com>
2022-05-26 07:56:48 -0500

    test: fix broken Header tests

--- src/app/common/components/header/header.component.spec.ts
+++ src/app/common/components/header/header.component.spec.ts
@@ -2,12 +2,15 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { HeaderComponent } from "./header.component";
 
+import { imports } from "src/app/app.module";
+
 describe("HeaderComponent", () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [...imports],
       declarations: [HeaderComponent],
     }).compileComponents();
   });
