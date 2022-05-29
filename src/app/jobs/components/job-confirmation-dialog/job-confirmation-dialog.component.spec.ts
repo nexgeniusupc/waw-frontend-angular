e2a794180d16c962e5c2b160174666f121e19668
Angel Meneses <angelmenesest123@gmail.com>
2022-05-29 00:57:21 -0500

    test: added missing imports

--- src/app/jobs/components/job-confirmation-dialog/job-confirmation-dialog.component.spec.ts
+++ src/app/jobs/components/job-confirmation-dialog/job-confirmation-dialog.component.spec.ts
@@ -2,12 +2,15 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { JobConfirmationDialogComponent } from "./job-confirmation-dialog.component";
 
+import { imports } from "src/app/app.module";
+
 describe("JobConfirmationDialogComponent", () => {
   let component: JobConfirmationDialogComponent;
   let fixture: ComponentFixture<JobConfirmationDialogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [...imports],
       declarations: [JobConfirmationDialogComponent],
     }).compileComponents();
   });
--- src/app/jobs/pages/jobs-search/jobs-search.component.spec.ts
+++ src/app/jobs/pages/jobs-search/jobs-search.component.spec.ts
@@ -2,12 +2,15 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { JobsSearchComponent } from "./jobs-search.component";
 
+import { imports } from "src/app/app.module";
+
 describe("JobsSearchComponent", () => {
   let component: JobsSearchComponent;
   let fixture: ComponentFixture<JobsSearchComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [...imports],
       declarations: [JobsSearchComponent],
     }).compileComponents();
   });
