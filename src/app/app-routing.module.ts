5e63eebf0324caf0aa977a9f38535337681b57aa
Angel Meneses <angelmenesest123@gmail.com>
2022-05-28 23:58:42 -0500

    feat(routes): added component routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ProfileComponent } from "./core/pages/profile/profile.component";
 import { SignInComponent } from "./auth/pages/signin/signin.component";
 import { SignUpComponent } from "./auth/pages/signup/signup.component";
 import { JobsComponent } from "./jobs/pages/jobs/jobs.component";
+import { JobsSearchComponent } from "./jobs/pages/jobs-search/jobs-search.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "account/profile", pathMatch: "full" },
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: "account/signin", component: SignInComponent },
   { path: "account/signup", component: SignUpComponent },
   { path: "account/jobs", component: JobsComponent },
+  { path: "account/search", component: JobsSearchComponent },
 ];
 
 @NgModule({
