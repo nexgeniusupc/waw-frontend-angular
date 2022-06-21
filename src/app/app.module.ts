d4ad62a879ef9cb1dc2744bdccc93b818ba614bc
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-06-20 20:28:14 -0500

    feat(offer): added the job add dialog component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { MatDialogModule } from "@angular/material/dialog";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { FooterComponent } from "./common/components/footer/footer.component";
+import { JobAddDialogComponent } from "./jobs/components/job-add-dialog/job-add-dialog.component";
 
 export const imports: NonNullable<NgModule["imports"]> = [
   AppRoutingModule,
@@ -70,6 +71,7 @@ export const imports: NonNullable<NgModule["imports"]> = [
     ResetpasswordComponent,
     ChangepasswordComponent,
     FooterComponent,
+    JobAddDialogComponent,
   ],
   imports: [BrowserModule, ...imports],
   providers: [],
