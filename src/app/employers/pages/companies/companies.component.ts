dedb86cecc19655b8297f2bc576a94a81e88bc1b
Angel Meneses <angelmenesest123@gmail.com>
2022-06-24 20:29:13 -0500

    feat: implemented component CRUD actions

--- src/app/employers/pages/companies/companies.component.ts
+++ src/app/employers/pages/companies/companies.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 import { Company } from "../../model/company";
 import { CompaniesService } from "../../services/companies.service";
 
@@ -8,9 +9,17 @@ import { CompaniesService } from "../../services/companies.service";
   styleUrls: ["./companies.component.css"],
 })
 export class CompaniesComponent implements OnInit {
-  // currentItem: Partial<Company> = {};
+  currentCompany: Partial<Company> = {};
   dataSource: Company[] = [];
+
+  @ViewChild("companiesForm", { static: false })
+  companiesForm!: NgForm;
+
   constructor(private companiesService: CompaniesService) {}
+
+  get isEditMode() {
+    return !!this.currentCompany.id;
+  }
 
   ngOnInit() {
     this.getAll();
@@ -21,15 +30,50 @@ export class CompaniesComponent implements OnInit {
       this.dataSource = response;
     });
   }
-  /*
+
   createCompany(company: Company) {
-    this.companiesService.create(company).subscribe();
+    this.companiesService.create(company).subscribe(response => {
+      this.dataSource = [...this.dataSource, response];
+    });
+  }
+
+  editCompany(company: Company) {
+    this.currentCompany = { ...company };
+  }
+
+  cancelEdit() {
+    this.currentCompany = {};
+    this.companiesForm.resetForm();
+  }
+
+  updateCompany(id: number, company: Company) {
+    this.companiesService.update(id, company).subscribe(response => {
+      this.dataSource = this.dataSource.map(current => {
+        if (current.id === id) return response;
+        return current;
+      });
+    });
   }
 
   deleteCompany(id: number) {
     this.companiesService.delete(id).subscribe(() => {
-
-    })
+      this.dataSource = this.dataSource.filter(current => current.id !== id);
+    });
   }
-  */
+
+  handleSubmit() {
+    console.log("handling submit...");
+    if (!this.companiesForm.form.valid) return;
+    console.log("passed validation...");
+    const company = this.currentCompany as Company;
+    if (this.isEditMode) {
+      console.log("sending update...");
+      this.updateCompany(company.id, company);
+    } else {
+      console.log("sending create...");
+      this.createCompany(company);
+    }
+    this.cancelEdit();
+    console.log("finished...");
+  }
 }
