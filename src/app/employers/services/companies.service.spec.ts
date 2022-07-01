fb864cf79aa14c7fd682a2c495b4e2613bf91f12
Angel Meneses <angelmenesest123@gmail.com>
2022-07-01 12:04:17 -0500

    fix: added http client injection for tests

--- src/app/employers/services/companies.service.spec.ts
+++ src/app/employers/services/companies.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from "@angular/common/http";
 import { TestBed } from "@angular/core/testing";
 
 import { CompaniesService } from "./companies.service";
@@ -6,7 +7,9 @@ describe("CompaniesService", () => {
   let service: CompaniesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(CompaniesService);
   });
 
