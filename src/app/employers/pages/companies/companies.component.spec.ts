ce8b5bc4eb085d9bb91204f5eafb47e28db2ed3d
Angel Meneses <angelmenesest123@gmail.com>
2022-07-01 11:57:51 -0500

    fix: updated spec file due to test failure

--- src/app/employers/pages/companies/companies.component.spec.ts
+++ src/app/employers/pages/companies/companies.component.spec.ts
@@ -2,12 +2,15 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { CompaniesComponent } from "./companies.component";
 
+import { imports } from "src/app/app.module";
+
 describe("CompaniesComponent", () => {
   let component: CompaniesComponent;
   let fixture: ComponentFixture<CompaniesComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [...imports],
       declarations: [CompaniesComponent],
     }).compileComponents();
   });
